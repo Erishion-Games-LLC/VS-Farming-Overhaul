f1799208730d12e8ad27952973e75d2a0087b456
Ben Colwell <52632729+Ben-Colwell@users.noreply.github.com>
2026-03-04 11:49:19 -0500

    REEEEEEEEEEE

--- FarmingOverhaul/FarmingOverhaul/FarmingOverhaulModSystem.cs
+++ FarmingOverhaul/FarmingOverhaul/FarmingOverhaulModSystem.cs
@@ -3,9 +3,9 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace Farming_Overhaul
+namespace FarmingOverhaul
 {
-    public class Farming_OverhaulModSystem : ModSystem
+    public class FarmingOverhaulModSystem : ModSystem
     {
 
         // Called on server and client
@@ -17,12 +17,12 @@ namespace Farming_Overhaul
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("farming overhaul:hello"));
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("farmingoverhaul:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("farming overhaul:hello"));
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("farmingoverhaul:hello"));
         }
 
     }
--- FarmingOverhaul/ZZCakeBuild/Program.cs
+++ FarmingOverhaul/ZZCakeBuild/Program.cs
@@ -26,7 +26,7 @@ namespace CakeBuild
 
     public class BuildContext : FrostingContext
     {
-        public const string ProjectName = "Farming_Overhaul";
+        public const string ProjectName = "FarmingOverhaul";
         public string BuildConfiguration { get; }
         public string Version { get; }
         public string Name { get; }

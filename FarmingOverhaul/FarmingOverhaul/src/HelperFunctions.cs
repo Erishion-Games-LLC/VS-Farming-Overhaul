358a78ae94298ddb72f23353113b327a6c73ae18
Ben Colwell <52632729+Ben-Colwell@users.noreply.github.com>
2026-03-29 13:27:46 -0400

    feat: update helperfunctions
    overhauls the existing math functions and adds more for use in sampling normal/gaussian distributions to both, get a random value and get a weighted value.

--- FarmingOverhaul/FarmingOverhaul/src/HelperFunctions.cs
+++ FarmingOverhaul/FarmingOverhaul/src/HelperFunctions.cs
@@ -1,43 +1,96 @@
 using System;
 using Vintagestory.API.Common.Entities;
 
-namespace FarmingOverhaul
+namespace FarmingOverhaul.src
 {
     public static class HelperFunctions
     {
-        public static int GenerateRandomInt(int min, int max)
+        /// Returns a random integer between min and max, inclusive.
+        public static int GenerateRandomIntFromMinMaxInclusive(Random random, int min, int max)
         {
-            return Random.Shared.Next(min, max + 1);
+            return random.Next(min, max + 1);
         }
 
-        public static double GenerateRandomDouble(double min, double max)
+        /// Returns a random integer between 0 and max, inclusive.
+        public static int GenerateRandomIntFromMaxInclusive(Random random, int max)
         {
-            return (max - min) * Random.Shared.NextDouble() + min;
+            return random.Next(max + 1);
+
         }
 
-        public static double GetDoubleFromNormalDistributionClamped(double min, double max)
+        /// Returns a random double between min and max, inclusive.
+        public static double GenerateRandomDouble(Random random, double min, double max)
+        {
+            return (max - min) * random.NextDouble() + min;
+        }
+
+        //Returns a random number based on the normal distribution with the given mean and standard deviation,
+        //but if the number is outside of the given range, it will keep generating until it gets one that is within the range.
+        //The standard deviation is calculated based on the range so that 99.7% of the numbers will be within the range.
+        public static double SampleNormalDistributionInRange(Random random, double min, double max)
         {
             double val;
             double standardDev = (max - min) / 6;
-            double mean = (max - min) / 2;
+            double mean = (max + min) / 2;
 
-            do { val = GenerateNormalDistribution(mean, standardDev); } 
+            do { val = SampleGaussian(random, mean, standardDev); } 
             while (val < min || val > max);
 
             return val;
         }
-
-        private static double GenerateNormalDistribution(double mean, double standardDev)
+               
+        public static double SampleNormalDistributionInRange(Random random, double min, double max, double mean, double stdDev)
         {
-            double var1 = 1.0 - Random.Shared.NextDouble();
-            double var2 = 1.0 - Random.Shared.NextDouble();
+            double val;
 
-            double randStdNormal =
-                Math.Sqrt(-2.0 * Math.Log(var1)) *
-                Math.Sin(2.0 * Math.PI * var2);
-            return mean + standardDev * randStdNormal;
+            do { val = SampleGaussian(random, mean, stdDev); }
+            while (val < min || val > max);
+            return val;
         }
 
+        /// <summary>
+        /// This method uses the Box-Muller transform to generate values from a standard normal
+        /// distribution, which are then scaled and shifted to match the specified parameters.</summary>
+        /// <param name="random">The random number generator to use for producing the sample. Cannot be null.</param>
+        /// <param name="mean">The mean, or expected value, of the normal distribution. Determines the center of the distribution.</param>
+        /// <param name="stdDev">The standard deviation of the normal distribution. Must be non-negative. Determines the spread of the
+        /// distribution.</param>
+        /// <returns>A double-precision floating-point number sampled from the normal distribution defined by the specified mean
+        /// and standard deviation.</returns>
+        public static double SampleGaussian(Random random, double mean, double stdDev)
+        {
+            return mean + stdDev * SampleStandardNormalDistribution(random);
+        }
+
+        /// <summary>
+        /// Generates a random value sampled from the standard normal distribution.
+        /// </summary>
+        /// <remarks>This method uses the Box-Muller transform to convert uniformly distributed random
+        /// numbers into a normally distributed value.
+        /// <param name="random">The random number generator used to produce the sample. Must not be null.</param>
+        /// <returns>A double-precision floating-point number representing a random sample from the standard normal distribution,
+        /// with a mean of 0 and a standard deviation of 1.</returns>
+        public static double SampleStandardNormalDistribution(Random random)
+        {
+            double var1 = 1.0 - random.NextDouble();
+            double var2 = 1.0 - random.NextDouble();
+
+            return Math.Sqrt(-2.0 * Math.Log(var1)) * Math.Cos(2.0 * Math.PI * var2);
+        }
+
+        public static double GetGaussianWeight(double value, double min, double max, double stdDevSpread = 6)
+        {
+            double mean = (max + min) / 2;
+            double stdDev = (max - min) / stdDevSpread;
+
+            if (stdDev <= 0) return value == mean ? 1.0 : 0.0;
+
+            double stdDevsFromCenter = (value - mean) / stdDev;
+
+            return Math.Exp(-0.5 * stdDevsFromCenter * stdDevsFromCenter);
+        }
+
+        //Returns the species of the entity as a lowercase string.
         public static string GetSpeciesStringLowerFromEntity(Entity entity)
         {
             return entity.Code.Path.Split('-')[0].ToLower();
